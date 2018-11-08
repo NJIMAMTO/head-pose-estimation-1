32c26a092cc0a61ba0387b0408b0051a3ab0c6cf
NJIMAMTO <s17018@g.metro-cit.ac.jp>
2018-11-08 16:15:38 +0900

    last update

--- video_test_shape.py
+++ video_test_shape.py
@@ -7,28 +7,28 @@ from imutils import face_utils
 
 face_landmark_path = 'shape_predictor_68_face_landmarks.dat'
 
-K = [6.5308391993466671e+002, 0.0, 3.1950000000000000e+002,
-     0.0, 6.5308391993466671e+002, 2.3950000000000000e+002,
+K = [6.5308391993466671e+002, 0.0, 320,
+     0.0, 6.5308391993466671e+002, 240,
      0.0, 0.0, 1.0]
 D = [7.0834633684407095e-002, 6.9140193737175351e-002, 0.0, 0.0, -1.3073460323689292e+000]
 
 cam_matrix = np.array(K).reshape(3, 3).astype(np.float32)
 dist_coeffs = np.array(D).reshape(5, 1).astype(np.float32)
 
-object_pts = np.float32([[6.825897, 6.760612, 4.402142],
-                         [1.330353, 7.122144, 6.903745],
-                         [-1.330353, 7.122144, 6.903745],
-                         [-6.825897, 6.760612, 4.402142],
-                         [5.311432, 5.485328, 3.987654],
-                         [1.789930, 5.393625, 4.413414],
-                         [-1.789930, 5.393625, 4.413414],
-                         [-5.311432, 5.485328, 3.987654],
-                         [2.005628, 1.409845, 6.165652],
-                         [-2.005628, 1.409845, 6.165652],
-                         [2.774015, -2.080775, 5.048531],
-                         [-2.774015, -2.080775, 5.048531],
-                         [0.000000, -3.116408, 6.097667],
-                         [0.000000, -7.415691, 4.070434]])
+object_pts = np.float32([[6.825897, 6.760612, 4.402142],    #0 eyebrow_start
+                         [1.330353, 7.122144, 6.903745],    #1 eyebrow_end
+                         [-1.330353, 7.122144, 6.903745],   #2 eyebrow_end
+                         [-6.825897, 6.760612, 4.402142],   #3 eyebrow_start
+                         [5.311432, 5.485328, 3.987654],    #4 eye_start
+                         [1.789930, 5.393625, 4.413414],    #5 eye_end
+                         [-1.789930, 5.393625, 4.413414],   #6 eye_end
+                         [-5.311432, 5.485328, 3.987654],   #7 eye_start
+                         [2.005628, 1.409845, 6.165652],    #8 nose_start
+                         [-2.005628, 1.409845, 6.165652],   #9 nose_end
+                         [2.774015, -2.080775, 5.048531],  #10 lip_start
+                         [-2.774015, -2.080775, 5.048531], #11 lip_end
+                         [0.000000, -3.116408, 6.097667],  #12 lip_uppper
+                         [0.000000, -7.415691, 4.070434]]) #13 lip_lower
 
 reprojectsrc = np.float32([[10.0, 10.0, 10.0],
                            [10.0, 10.0, -10.0],
@@ -66,13 +66,7 @@ def get_head_pose(shape):
 
 #====================#Homography Transformation of face#====================#
 
-def HT_head_pose(frame,shape):
-    rotation_vec = np.float32([0,0,0])
-    translation_vec = np.float32([0,0,0])
-    
-    #reproject_obj_pts, _  = cv2.projectPoints(object_pts, rotation_vec, translation_vec, cam_matrix,dist_coeffs)
-    #reproject_obj_pts = tuple(map(tuple, reproject_obj_pts.reshape(14, 2)))
-
+def HT_head_pose(frame,shape): 
     reproject_obj_pts = np.float32([[object_pts[0][0],object_pts[0][1]],
                                     [object_pts[1][0],object_pts[1][1]],
                                     [object_pts[2][0],object_pts[2][1]],
@@ -86,26 +80,29 @@ def HT_head_pose(frame,shape):
                                     [object_pts[10][0],object_pts[10][1]],
                                     [object_pts[11][0],object_pts[11][1]],
                                     [object_pts[12][0],object_pts[12][1]],
-                                    [object_pts[13][0],object_pts[13][1]]])
-    reproject_obj_pts = reproject_obj_pts * 50
+                                    [object_pts[13][0],object_pts[13][1]]])  
+    reproject_obj_pts = reproject_obj_pts * -30
+    image_pts = np.float32([shape[17], shape[21], shape[22], shape[26], shape[36],
+                            shape[39], shape[42], shape[45], shape[31], shape[35],
+                            shape[48], shape[54], shape[57], shape[8]])
 
-    pts_obj = np.float32([reproject_obj_pts[9], reproject_obj_pts[8], reproject_obj_pts[6], reproject_obj_pts[5]])
-    pts_shape = np.float32([shape[39], shape[42], shape[31], shape[35]])
-    homography_mat = cv2.getPerspectiveTransform(pts_obj,pts_shape)
+    #homography_mat,_ = cv2.findHomography(reproject_obj_pts, image_pts, 0, 3)
+    homography_mat,_ = cv2.findHomography(image_pts, reproject_obj_pts, 0, 3)
+    proj_point = cv2.perspectiveTransform(np.array([shape.astype('float32')]), homography_mat)
+    proj_point = tuple(map(tuple, proj_point.reshape(68, 2)))
 
+    #====================set white image=====================#
     rows = 1024
     cols = 1024
     img = np.zeros((rows, cols, 3), np.uint8)
     img[:,:,:] = [255,255,255]
 
-    for (x, y) in shape:
-        point = np.array([[x],
-                          [y],
-                          [1]])
-        proj_point = np.dot(np.linalg.inv(homography_mat), point)
-        cv2.circle(img, (proj_point[0] + 512, proj_point[1] + 512), 1, (0, 0, 0), -1)
+    for (xs, ys),(xp, yp) in zip(shape, proj_point):
+        cv2.circle(img, (int(xp) + 320, int(yp) + 240), 3, (0, 0, 0), -1)
+        cv2.circle(img, (xs + 512, ys + 512), 1, (0, 0, 0), -1)
     
     cv2.imshow("test", img)
+    #====================end====================#
     return 0
 #====================#               end               #====================#
 
@@ -120,7 +117,6 @@ def main():
 
     while cap.isOpened():
         ret, frame = cap.read()
-
         if ret:
             face_rects = detector(frame, 0)
 
